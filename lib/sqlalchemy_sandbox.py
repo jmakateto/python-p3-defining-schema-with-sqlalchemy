d2d5b01c5a0d5c1bef1dbc7bc0b493eac7231b7d
john makateto <jmakateto@gmail.com>
2023-09-10 23:50:23 +0300

    made changes

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -2,11 +2,36 @@
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker  # Import the sessionmaker
 
 Base = declarative_base()
 
+# Define the Student class and associated table
 class Student(Base):
-    pass
+    __tablename__ = 'students'  # Set the table name
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    # Create a database engine
+    engine = create_engine('sqlite:///students.db')  # Use SQLite as the database
+
+    # Create the tables in the database
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)  # Bind the session to the engine
+    session = Session()
+
+    # Example: Adding a student to the database
+    new_student = Student(name="John Doe")
+    session.add(new_student)
+    session.commit()  # Commit the transaction
+
+    # Example: Querying the database
+    students = session.query(Student).all()
+    for student in students:
+        print(f"Student ID: {student.id}, Name: {student.name}")
+
+    session.close()  # Close the session
